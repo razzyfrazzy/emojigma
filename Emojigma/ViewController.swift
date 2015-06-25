8bf98c0fbb0e292e13569cfc4245e1e6cf97173f
Raiza Abubakar <raizaabubakar@Raizas-MacBook-Pro.local>
2015-06-24 22:18:27 -0700

    Initial Commit

---
+++ Emojigma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emojigma
+//
+//  Created by Raiza Abubakar on 6/24/15.
+//  Copyright (c) 2015 Ghost Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
