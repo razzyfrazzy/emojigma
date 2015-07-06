28f90fbee852a917fdd88769e4ca67f612b2ad72
Raiza Abubakar <raizaabubakar@Raizas-MacBook-Pro.local>
2015-07-06 12:53:56 -0700

    test

--- Emojigma/AppDelegate.swift
+++ Emojigma/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    // Comment
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         println("application didFinishLaunching")
